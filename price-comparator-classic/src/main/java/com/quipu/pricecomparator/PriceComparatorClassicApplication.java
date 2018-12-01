f824e694bb3e2945751a618cca820c36e51762b0
mwolodkiewicz <mwolodkiewicz@eworklabs.io>
2018-12-01 13:41:35 +0100

    3 modules added:  - price comparator classic  - price comparator react  - price provider

---
+++ price-comparator-classic/src/main/java/com/quipu/pricecomparator/PriceComparatorClassicApplication.java
@@ -0,0 +1,12 @@
+package com.quipu.pricecomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparatorClassicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceComparatorClassicApplication.class, args);
+    }
+}
---
+++ price-comparator-react/src/main/java/com/quipu/pricecomparator/PriceComparatorReactApplication.java
@@ -0,0 +1,12 @@
+package com.quipu.pricecomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceComparatorReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceComparatorReactApplication.class, args);
+    }
+}
