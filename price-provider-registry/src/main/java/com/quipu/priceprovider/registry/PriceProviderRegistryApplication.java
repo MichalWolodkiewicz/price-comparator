8b62599ad1fbbfda6f70812423d78c54f90972a1
MichalWolodkiewicz <michal@quipu.software>
2018-12-01 15:43:42 +0100

    Price provider registry module added.

---
+++ price-provider-registry/src/main/java/com/quipu/priceprovider/registry/PriceProviderRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.quipu.priceprovider.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceProviderRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceProviderRegistryApplication.class, args);
+    }
+}
